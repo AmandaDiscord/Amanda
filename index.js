127ff5d0cd53267dba1b2ac641d188ca17d789cb
bitsnake <bradleyjplatt@yahoo.com>
2018-05-06 19:03:01 -0700

    Tidy the new all addition to commands

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ const commands = {
           const embed = new Discord.RichEmbed()
             .setDescription(`**${msg.author.tag}**, I couldn't find the help pane for that command`)
             .setColor("B60000")
-          return msg.channel.send({embed});
+          return msg.channel.send(`${msg.author.username}, I couldn't find the help pane for that command`)
         }
         const embed = new Discord.RichEmbed()
           .addField(`Help for ${cmd}:`, `Usage: ${usage}\nDescription: ${description}`)
@@ -111,7 +111,7 @@ const commands = {
       else {
         const embed = new Discord.RichEmbed()
           .setAuthor("Command Categories:")
-          .setDescription(`❯ Core\n❯ Statistics\n❯ Gambling\n❯ Guild\n❯ Fun\n❯ Search\n❯ Images\n❯ Music\n❯ NSFW\n\n:information_source: **Typing \`${Config.commandPrefix}commands <category>\` will get you a list of all of the commands in that category. Ex: ${Config.commandPrefix}commands core**`)
+          .setDescription(`❯ Core\n❯ Statistics\n❯ Gambling\n❯ Guild\n❯ Fun\n❯ Search\n❯ Images\n❯ Music\n❯ NSFW\n\n:information_source: **Typing \`${Config.commandPrefix}commands <category>\` will get you a list off all of the commands in that category. Ex: \`${Config.commandPrefix}commands core\`. Also typing \`${Config.commandPrefix}commands all\` will return all of the available commands**`)
           .setFooter("Amanda help pane", djs.user.avatarURL)
           .setColor('36393E')
         msg.author.send({embed}).catch(() => msg.channel.send(`${msg.author.username}, you must allow me to DM you for this command to work.`));
@@ -167,26 +167,20 @@ const commands = {
           .setDescription(`${Config.commandPrefix}cat\n${Config.commandPrefix}dog\n${Config.commandPrefix}space`)
           .setColor('36393E')
         msg.author.send({embed}).catch(() => msg.channel.send(`${msg.author.username}, you must allow me to DM you for this command to work.`));
-      } else if (suffix.toLowerCase == "all") {
+      } else if (suffix.toLowerCase() == "all") {
         const embed = new Discord.RichEmbed()
-          .setAuthor("Available Command List:")
-          .addField("Core Commands:", "&help <command>\n&commands <category>\n&invite\n&info\n&privacy")
-          .addBlankField(true)
-          .addField("Statistic Commands:", "&stats\n&ping\n&uptime")
-          .addBlankField(true)
-          .addField("Casino Commands:", "&dice\n&flip\n&bf <amount> <side>\n&slot <amount>\n&coins <user>\n&mine\n&lb")
-          .addBlankField(true)
-          .addField("Image Commands:", "&cat\n&dog\n&space")
-          .addBlankField(true)
-          .addField("Guild Commands:", "&user <user>\n&tidy <# to delete>\n&emoji <:EMOJI:>\n&emojilist\n&guild")
-          .addBlankField(true)
-          .addField("Interaction Commands:", "&poke <user>\n&boop <user>\n&hug <user>\n&cuddle <user>\n&pat <user>\n&kiss <user>\n&slap <user>\n&stab <user>\n&nom <user>")
-          .addBlankField(true)
-          .addField("Fun Commands:", "&norris\n&randnum <min#> <max#>\n&yn <question>\n&ball <question>\n&rate <thing to rate>")
-          .addBlankField(true)
-          .addField("Search Commands:", "&urban <search terms>")
+          .setAuthor(`Full command list`)
+          .addField(`**❯ Core:**`, `${Config.commandPrefix}help <command>\n${Config.commandPrefix}commands <category>\n${Config.commandPrefix}invite\n${Config.commandPrefix}info\n${Config.commandPrefix}privacy`)
+          .addField(`**❯ Statistics:**`, `${Config.commandPrefix}ping\n${Config.commandPrefix}uptime\n${Config.commandPrefix}stats`)
+          .addField(`**❯ Gambling:**`, `${Config.commandPrefix}coins <user>\n${Config.commandPrefix}slot <amount>\n${Config.commandPrefix}flip\n${Config.commandPrefix}bf <amount> <side>\n${Config.commandPrefix}lb\n${Config.commandPrefix}mine\n${Config.commandPrefix}dice`)
+          .addField(`**❯ Guild:**`, `*Moderation:*\n${Config.commandPrefix}tidy <# to delete>\n*Information:*\n${Config.commandPrefix}guild\n${Config.commandPrefix}user <user>\n${Config.commandPrefix}emoji <:emoji:>\n${Config.commandPrefix}emojilist\n*Interaction:*\n${Config.commandPrefix}poke <user>\n${Config.commandPrefix}boop <user>\n${Config.commandPrefix}hug <user>\n${Config.commandPrefix}cuddle <user>\n${Config.commandPrefix}pat <user>\n${Config.commandPrefix}kiss <user>\n${Config.commandPrefix}slap <user>\n${Config.commandPrefix}stab <user>\n${Config.commandPrefix}nom <user>`)
+          .addField(`**❯ Fun:**`, `${Config.commandPrefix}norris\n${Config.commandPrefix}randnum <min#> <max#>\n${Config.commandPrefix}yn <question>\n${Config.commandPrefix}ball <question>\n${Config.commandPrefix}rate <thing to rate>`)
+          .addField(`**❯ Search:**`, `${Config.commandPrefix}urban <search terms>`)
+          .addField(`**❯ Images:**`, `${Config.commandPrefix}cat\n${Config.commandPrefix}dog\n${Config.commandPrefix}space`)
+          .addField(`**❯ Music:**`, `Null`)
+          .addField(`**❯ NSFW:**`, `Null`)
           .setColor('36393E')
-          .setFooter("Amanda help pane", djs.user.avatarURL)
+          .setFooter("Old Amanda help pane", djs.user.avatarURL)
         msg.author.send({embed}).catch(() => msg.channel.send(`${msg.author.username}, you must allow me to DM you for this command to work.`));
       } else {
         const embed = new Discord.RichEmbed()
