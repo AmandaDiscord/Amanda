09e89f3ae9d40110d7cab4d63f1afe188f9436bf
Unknown <cheweyzak002@gmail.com>
2018-06-21 16:51:23 +1200

    added express

--- index-ama.js
+++ index-ama.js
@@ -15,6 +15,14 @@ const djs = client.djsClient();
 
 console.log(`Starting`);
 
+const PORT = process.env.PORT || 5000
+const express = require('express')
+const app = express()
+app.get('/', function (req, res) {
+  res.send('Online')
+})
+app.listen(PORT)
+
 let db = require("./database.js")();
 let passthrough = { Discord, client, djs, dio, reloadEvent, utils, db, commands };
 require("./plugins.js")(passthrough);
