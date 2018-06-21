12307bb65647e2892a0c57506e297328348a6f2e
Unknown <cheweyzak002@gmail.com>
2018-06-21 16:53:13 +1200

    missed an auth

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ module.exports = function() {
 	return mysql.createPool({
 		host: "cadence.gq",
 		user: "amanda",
-		password: require("./auth.json").mysql_password,
+		password: (process.env.is_heroku)? process.env.auth:JSON.parse(fs.readFileSync("./auth.json", "utf8")),
 		database: "money",
 		connectionLimit: 5
 	});
