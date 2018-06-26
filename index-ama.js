53c25d0d148f89b53b305d678718588c0bdae4cc
Edward Fish <cloudrac3r@vivaldi.net>
2018-06-26 19:39:11 +1200

    Remove express

--- index-ama.js
+++ index-ama.js
@@ -15,13 +15,7 @@ const djs = client.djsClient();
 
 console.log(`Starting`);
 
-const PORT = process.env.PORT || 5000
-const express = require('express')
-const app = express()
-app.get('/', function (req, res) {
-  res.send('Online')
-})
-app.listen(PORT)
+if (process.env.is_heroku) require("http").createServer(new Function(), process.env.PORT);
 
 let db = require("./database.js")();
 let passthrough = { Discord, client, djs, dio, reloadEvent, utils, db, commands };
