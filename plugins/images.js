adcf035468032e78bce8128fdb0d8549d2bdc099
snakke <bradleyjplatt@yahoo.com>
2018-07-19 23:59:08 -0700

    Added neko command

--- plugins/images.js
+++ plugins/images.js
@@ -8,7 +8,7 @@ module.exports = function(passthrough) {
 			description: "Returns an image of a cute cat",
 			aliases: ["cat"],
 			category: "images",
-			process: function(msg, suffix) {
+			process: function(msg) {
 				request("https://api.cheweybot.ga/cat", function(err, res, body) {
 					if (err) return msg.channel.send(`Error... API returned nothing`);
 					try {
@@ -30,7 +30,7 @@ module.exports = function(passthrough) {
 			description: "Returns an image of a cute doggo",
 			aliases: ["dog", "doggo"],
 			category: "images",
-			process: function(msg, suffix) {
+			process: function(msg) {
 				request("https://api.cheweybot.ga/dog", function(err, res, body) {
 					if (err) return msg.channel.send("Error. The API returned nothing...");
 					try {
@@ -52,7 +52,7 @@ module.exports = function(passthrough) {
 			description: "Returns an image of space",
 			aliases: ["space"],
 			category: "images",
-			process: function(msg, suffix) {
+			process: function(msg) {
 				request("https://api.cheweybot.ga/space", function(err, res, body) {
 					if (err) return msg.channel.send("Error... API returned nothing");
 					try {
@@ -74,7 +74,7 @@ module.exports = function(passthrough) {
 			description: "Returns a meme",
 			aliases: ["meme"],
 			category: "images",
-			process: async function(msg, suffix) {
+			process: async function(msg) {
 				var array = ["dankmemes", "meirl", "2meirl4meirl", "animemes", "sbubby", "fellowkids", "bertstrips", "2healthbars", "coaxedintoasnafu", "bossfight"];
 				var choice = array[Math.floor(Math.random() * array.length)];
 				request({ url: `https://api.reddit.com/r/${choice}/random`, headers: { "User-Agent": "Amanda" } }, function(err, res, body) {
@@ -98,7 +98,7 @@ module.exports = function(passthrough) {
 			description: "Returns an image of a snek",
 			aliases: ["snek", "snake"],
 			category: "images",
-			process: function(msg, suffix) {
+			process: function(msg) {
 				request("https://api.cheweybot.ga/snake", function(err, res, body) {
 					if (err) return msg.channel.send(`There was an error:\n${err}`);
 					try {
@@ -120,7 +120,7 @@ module.exports = function(passthrough) {
 			description: "Returns an image of a birb",
 			aliases: ["birb", "bird"],
 			category: "images",
-			process: function(msg, suffix) {
+			process: function(msg) {
 				request("https://api.cheweybot.ga/birb", function(err, res, body) {
 					if (err) return msg.channel.send(`There was an error:\n${err}`);
 					try {
@@ -132,6 +132,28 @@ module.exports = function(passthrough) {
 						.setImage(data.data)
 						.setColor('36393E')
 						.setFooter("Powered by api.cheweybot.ga")
+					msg.channel.send({embed});
+				});
+			}
+		},
+
+		"neko": {
+			usage: "",
+			description: "Returns an image of a neko (ฅ’ω’ฅ)",
+			aliases: ["neko"],
+			category: "images",
+			process: function(msg) {
+				request("https://nekos.life/api/v2/img/neko", function(err, res, body) {
+					if (err) return msg.channel.send(`There was an error:\n${err}`);
+					try {
+						var data = JSON.parse(body);
+					} catch (error) {
+						return msg.channel.send(`Error while requesting an image of a neko\n${error}`);
+					}
+					const embed = new Discord.RichEmbed()
+						.setImage(data.url)
+						.setColor('36393E')
+						.setFooter("Powered by nekos.life")
 					msg.channel.send({embed});
 				});
 			}
