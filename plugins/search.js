4ec10031228ad6e57a7fe36cb936e722222ee0d0
Edward Fish <cloudrac3r@vivaldi.net>
2018-07-20 17:21:52 +1200

    Fix embed limit exceed crash (probably)

--- plugins/search.js
+++ plugins/search.js
@@ -1,3 +1,5 @@
+const rp = require("request-promise");
+
 module.exports = function(passthrough) {
 	const { Discord, client } = passthrough;
 	return {
@@ -6,27 +8,25 @@ module.exports = function(passthrough) {
 			description: "Searches the urban dictionary for a term",
 			aliases: ["urban", "define"],
 			category: "search",
-			process: function(msg, suffix) {
+			process: async function(msg, suffix) {
 				let req = undefined;
 				if (msg.channel.type == "dm") req = true;
 				else if (msg.channel.nsfw) req = true;
 				else req = false;
 				if (!req) return msg.channel.send(`Due to abuse and bot listing rules, this command is only allowed in nsfw channels`);
-				require("request")(`http://api.urbandictionary.com/v0/define?term=${suffix}`, function(err, res, body) {
-					if (err) return msg.channel.send("Error... API returned nothing.");
-					try {
-						var data = JSON.parse(body);
-					} catch (error) {
-						return msg.channel.send(`Error while requesting the definition\n${error}`);
-					}
+				let body = await rp(`http://api.urbandictionary.com/v0/define?term=${suffix}`);
+				try {
+					let data = JSON.parse(body);
 					if (data.result_type == "no_results") return msg.channel.send(`${msg.author.username}, those are invalid search terms`);
 					const embed = new Discord.RichEmbed()
 						.setAuthor(data.list[0].word || suffix)
-						.addField("Definition:", data.list[0].definition || "Not available")
-						.addField("Example:", data.list[0].example || "Not available")
+						.addField("Definition:", (data.list[0].definition || "Not available").slice(0, 1024))
+						.addField("Example:", (data.list[0].example || "Not available").slice(0, 1024))
 						.setColor("36393E");
 					msg.channel.send({embed})
-				});
+				} catch (error) {
+					return msg.channel.send(`Error while requesting the definition\n${error}`);
+				}
 			}
 		}
 	}
