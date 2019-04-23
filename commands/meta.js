2655e6ef3aeec09f4201d52357cfe38bff71d80d
snakke <bradleyjplatt@yahoo.com>
2019-04-23 09:46:16 -0700

    Asset crunching and bug fixes

--- commands/meta.js
+++ commands/meta.js
@@ -117,7 +117,7 @@ module.exports = function(passthrough) {
 						`${c1.tag} <:bravery:479939311593324557> <:NitroBadge:421774688507920406>\n`+
 						`${c2.tag} <:brilliance:479939329104412672> <:NitroBadge:421774688507920406>`)
 					.addField("Code", `[node.js](https://nodejs.org/) ${process.version} + [discord.js](https://www.npmjs.com/package/discord.js) ${Discord.version}`)
-					.addField("Links", `Visit Amanda's [website](${config.website_protocol}://${config.website_domain}/) or her [support server](https://discord.gg/zhthQjH)\nYou can also visit her listing sites at [Discord Bot List](https://discordbots.org/bot/405208699313848330) or on [Discord Bots](https://bots.discord.pw/bots/405208699313848330)`)
+					.addField("Links", `Visit Amanda's [website](${config.website_protocol}://${config.website_domain}/) or her [support server](https://discord.gg/zhthQjH)`)
 					.setColor("36393E");
 				return msg.channel.send(embed);
 			}
