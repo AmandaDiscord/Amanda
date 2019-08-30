f5ffd26f301c99e24c2b1aa672dcefff8aedf5cf
Cadence Fish <cloudrac3r@vivaldi.net>
2019-08-30 15:39:54 +1200

    Add sample config

---
+++ config.sample.js
@@ -0,0 +1,15 @@
+module.exports = {
+	bot_token: "discord bot token here",
+	fake_token: "write something here to be displayed when the token is evaled",
+	mysql_domain: "amanda.discord-bots.ga",
+	mysql_password: "mysql password here",
+	yt_api_key: "youtube api key here",
+	dbl_key: null,
+	prefixes: ["array", "of", "initial", "bot", "prefixes"],
+	chewey_api_key: "chewey bot api key here",
+	website_protocol: "http",
+	website_domain: "localhost:10400",
+	is_staging: true,
+	music_dash_enabled: false,
+	lavalink_password: "lavalink password here"
+}
