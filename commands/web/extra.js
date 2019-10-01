d4e874ce8737c0f00b6b0b941c0861114a93a9a4
Cadence Fish <cloudrac3r@vivaldi.net>
2019-10-01 15:29:12 +1300

    Hotfix for CSRF, revert before website merge

--- commands/web/extra.js
+++ commands/web/extra.js
@@ -47,7 +47,8 @@ module.exports = function(passthrough) {
 
 		generateCSRF: function(loginToken = null) {
 			let token = crypto.randomBytes(32).toString("hex")
-			utils.sql.all("INSERT INTO CSRFTokens (token, loginToken) VALUES (?, ?)", [token, loginToken])
+			let expires = Date.now() + 6*60*60*1000 // 6 hours
+			utils.sql.all("INSERT INTO CSRFTokens (token, loginToken, expires) VALUES (?, ?, ?)", [token, loginToken, expires])
 			return token
 		},
 
@@ -56,6 +57,8 @@ module.exports = function(passthrough) {
 			let row = await utils.sql.get("SELECT * FROM CSRFTokens WHERE token = ?", token)
 			// Token doesn't exist? Fail.
 			if (!row) result = false
+			// Token expired? Fail.
+			else if (row.expires < Date.now()) result = false
 			// Checking against a loginToken, but row loginToken differs? Fail.
 			else if (loginToken && row.loginToken != loginToken) result = false
 			// Looking good.
