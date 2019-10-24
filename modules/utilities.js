7437acfab9a5d07e8e488fe52ee962839f5e294c
Papi <bradleyjplatt@yahoo.com>
2019-10-24 00:36:10 -0700

    add whitespace after timestring formatting

--- modules/utilities.js
+++ modules/utilities.js
@@ -842,9 +842,9 @@ const utils = {
 		number -= mins * 1000 * 60
 		const secs = Math.floor(number / 1000)
 		let timestr = ""
-		if (days > 0) timestr += `${days} d`
-		if (hours > 0) timestr += `${hours} h`
-		if (mins > 0) timestr += `${mins} m`
+		if (days > 0) timestr += `${days} d `
+		if (hours > 0) timestr += `${hours} h `
+		if (mins > 0) timestr += `${mins} m `
 		if (secs > 0) timestr += `${secs} s`
 		return timestr
 	},
