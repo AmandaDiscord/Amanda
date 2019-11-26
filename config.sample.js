6ff63e5d8e4140514e5ae3ace737dcf1ff966384
Cadence Fish <cloudrac3r@vivaldi.net>
2019-11-27 01:18:30 +1300

    Format config.sample.js

--- config.sample.js
+++ config.sample.js
@@ -1,18 +1,20 @@
 module.exports = {
+	// Keys
 	bot_token: "discord bot token here",
-	fake_token: "write something here to be displayed when the token is evaled",
-	mysql_domain: "amanda.discord-bots.ga",
 	mysql_password: "mysql password here",
 	yt_api_key: "youtube api key here",
-	dbl_key: null,
-	prefixes: ["array", "of", "initial", "bot", "prefixes"],
 	chewey_api_key: "chewey bot api key here",
+	lavalink_password: "lavalink password here",
+
+	// Environment
+	mysql_domain: "amanda.discord-bots.ga",
 	website_protocol: "http",
 	website_domain: "localhost:10400",
 	website_ipc_bind: "localhost",
-	is_staging: true,
-	music_dash_enabled: false,
-	lavalink_password: "lavalink password here",
 	invidious_origin: "https://invidio.us",
-	use_invidious: false
+
+	// Settings
+	music_dash_enabled: true,
+	use_invidious: false,
+	fake_token: "write something here to be displayed when the token is evaled"
 }
