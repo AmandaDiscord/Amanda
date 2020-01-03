bde255e64848bdce69161bed0aea09577e3c82b2
Cadence Fish <cloudrac3r@vivaldi.net>
2020-01-04 02:04:37 +1300

    Add /to/ redirects

--- website/api/redirects.js
+++ website/api/redirects.js
@@ -5,12 +5,21 @@ function redirect(statusCode, location) {
 			"Location": location
 		},
 		contentType: "text/html",
-		content: "Redirecting..."
+		content: `Redirecting to <a href="${location}">${location}</a>...`
 	}
 }
 
 module.exports = [
-	{ route: "/stats", methods: ["GET"], code: () => {
-		return Promise.resolve(redirect(302, "https://cheweyz.github.io/discord-bot-analytics-dash/index.html?id=320067006521147393"))
-	} }
-]
+	["/stats", "https://cheweyz.github.io/discord-bot-analytics-dash/index.html?id=320067006521147393"], // legacy, feel free to replace in future
+	["/to/stats", "https://cheweyz.github.io/discord-bot-analytics-dash/index.html?id=320067006521147393"],
+	["/to/patreon", "https://www.patreon.com/papiophidian"],
+	["/to/paypal", "https://paypal.me/papiophidian"],
+	["/to/server", "https://discord.gg/zhthQjH"],
+	["/to/add", "https://discordapp.com/oauth2/authorize?client_id=405208699313848330&permissions=57344&scope=bot"]
+].map(entry => ({
+	route: entry[0],
+	methods: ["GET"],
+	code: () => {
+		return Promise.resolve(redirect(302, entry[1]))
+	}
+}))
