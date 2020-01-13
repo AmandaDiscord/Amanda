e3d110c4c1b7ac452da0d479f99bc324cadc89b7
Cadence Fish <cloudrac3r@vivaldi.net>
2020-01-14 04:18:42 +1300

    Remove unused reject parameter

--- website/modules/services/services.js
+++ website/modules/services/services.js
@@ -4,7 +4,7 @@ ipc.replier.addReceivers([
 	["REPLY_GET_GUILD_MEMBER", {
 		op: "GET_GUILD_MEMBER",
 		fn: ({ userID, guildID }) => {
-			return new Promise((resolve, reject) => {
+			return new Promise(resolve => {
 				snow.guild.getGuildMember(guildID, userID)
 				.then(result => resolve({ status: "ok", data: result }))
 				.catch(e => resolve({ status: "error", data: e }))
