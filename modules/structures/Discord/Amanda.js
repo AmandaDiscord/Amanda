1e4e11d5de839dca9c278857e663b1f06593aed1
Papi <bradleyjplatt@yahoo.com>
2020-03-17 16:22:40 -0700

    Modify LavaLink region map

--- modules/structures/Discord/Amanda.js
+++ modules/structures/Discord/Amanda.js
@@ -17,8 +17,8 @@ class Amanda extends Discord.Client {
 
 		/** @type {Discord.Collection<string, { name: string, host: string, regions: Array<string> }>} */
 		this.regionMap = new Discord.Collection()
-		this.regionMap.set("americas", { name: "Pencil", host: "amanda.moe", regions: ["brazil", "us-central", "us-south", "us-east", "us-south"] })
-		this.regionMap.set("asia", { name: "Crayon", host: "139.99.90.94", regions: ["eu-west", "hongkong", "japan", "singapore", "southafrica", "eu-central", "europe", "india", "russia", "south-korea", "sydney"] })
+		this.regionMap.set("americas", { name: "Pencil", host: "amanda.moe", regions: ["brazil", "us-central", "us-south", "us-east", "us-west", "eu-central", "europe", "eu-west", "sydney", "southafrica"] })
+		this.regionMap.set("asia", { name: "Crayon", host: "139.99.90.94", regions: ["hongkong", "japan", "singapore", "india", "russia", "south-korea"] })
 
 		/**
 		 * Do not use this
