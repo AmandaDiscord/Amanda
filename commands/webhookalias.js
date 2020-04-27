2911e4bf068d4a22eaefca0fcda1c555d591bc95
Papi <bradleyjplatt@yahoo.com>
2020-04-26 22:53:39 -0700

    lint webhookalias command

--- commands/webhookalias.js
+++ commands/webhookalias.js
@@ -18,36 +18,36 @@ commands.assign([
 		process(msg, suffix) {
 			const messageID = suffix
 			new Promise((resolve, reject) => {
-				if (!messageID.match(/^\d+$/)) reject("Not a number")
+				if (!messageID.match(/^\d+$/)) reject(new Error("Not a number"))
 				resolve(msg.channel.messages.fetch(messageID))
 			}).then(
 				/** @param {import("discord.js").Message} link */
-			async link => {
-				if (!link.webhookID) {
-					msg.channel.send("That message wasn't sent by a webhook.")
-				} else {
-					const checkString = crypto.randomBytes(12).toString("hex")
-					const checkMessage = await msg.channel.send("To confirm, the webhook must now send this message: `" + checkString + "`")
-					const collector = msg.channel.createMessageCollector(
-						m => m.webhookID === link.webhookID && m.author.username === link.author.username,
-						{ max: 1, time: 30000 }
-					)
-					collector.next.then(confirmation => {
-						if (confirmation.content !== checkString) {
-							msg.channel.send("That's not the right confirmation code.")
-						} else {
-							utils.sql.all(
-								"INSERT INTO WebhookAliases (webhookID, webhook_username, userID, user_username, user_discriminator)"
+				async link => {
+					if (!link.webhookID) {
+						msg.channel.send("That message wasn't sent by a webhook.")
+					} else {
+						const checkString = crypto.randomBytes(12).toString("hex")
+						const checkMessage = await msg.channel.send("To confirm, the webhook must now send this message: `" + checkString + "`")
+						const collector = msg.channel.createMessageCollector(
+							m => m.webhookID === link.webhookID && m.author.username === link.author.username,
+							{ max: 1, time: 30000 }
+						)
+						collector.next.then(confirmation => {
+							if (confirmation.content !== checkString) {
+								msg.channel.send("That's not the right confirmation code.")
+							} else {
+								utils.sql.all(
+									"INSERT INTO WebhookAliases (webhookID, webhook_username, userID, user_username, user_discriminator)"
 									+ " VALUES (?, ?, ?, ?, ?)",
-								[link.webhookID, link.author.username, msg.author.id, msg.author.username, msg.author.discriminator]
-							)
-							msg.channel.send("Alias created. The webhook can now act as you.")
-						}
-					}).catch(() => {
-						checkMessage.edit("Timed out.")
-					})
-				}
-			}).catch(() => {
+									[link.webhookID, link.author.username, msg.author.id, msg.author.username, msg.author.discriminator]
+								)
+								msg.channel.send("Alias created. The webhook can now act as you.")
+							}
+						}).catch(() => {
+							checkMessage.edit("Timed out.")
+						})
+					}
+				}).catch(() => {
 				msg.channel.send("That's not a valid message ID.")
 			})
 		}
