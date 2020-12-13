ade73cb6d3c58f00042ef53a9ee2aa5bb1c7d9c9
Papi <bradleyjplatt@yahoo.com>
2020-12-12 23:47:47 -0700

    handle unhandled rejections

--- workers/rest.js
+++ workers/rest.js
@@ -6,3 +6,5 @@ const [domain, port] = config.rest_server_domain.split(":")
 const gate = new Gate({ host: domain, port: Number(port), token: config.bot_token, options: { disableEveryone: true } })
 
 gate.start().then(() => console.log("REST Initialized"))
+
+process.on("unhandledRejection", console.error)
