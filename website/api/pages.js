b831120fbe55ef4efeb4033e1a6d50ede9dabb4e
Papi <bradleyjplatt@yahoo.com>
2021-07-20 00:34:30 -0700

    snap website/dream

--- website/api/pages.js
+++ website/api/pages.js
@@ -30,11 +30,6 @@ module.exports = [
 		}
 	},
 	{
-		route: "/dream", methods: ["GET"], code: async () => {
-			return render(200, "pug/dream.pug")
-		}
-	},
-	{
 		route: "/donate", methods: ["GET"], code: async () => {
 			return render(200, "pug/donate.pug")
 		}
