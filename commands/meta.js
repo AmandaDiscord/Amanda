761e3dda386ef1806c3c0a18679e08c454d974b0
Papi <bradleyjplatt@yahoo.com>
2021-07-22 02:34:47 -0700

    Pages don't start at 1 lol mb

--- commands/meta.js
+++ commands/meta.js
@@ -196,7 +196,7 @@ commands.assign([
 			} else if (suffix.toLowerCase() === "lavalink" || suffix.toLowerCase() === "ll") {
 				const nodeIDs = [...client.lavalink.nodes.keys()]
 				return utils.paginate(msg.channel, nodeIDs.length, (page) => {
-					const key = nodeIDs[page - 1]
+					const key = nodeIDs[page]
 					const node = client.lavalink.nodes.get(key)
 					if (!node) return utils.contentify(msg.channel, new Discord.MessageEmbed().setColor(constants.standard_embed_color).addFields([{ name: "Deleted Node", value: "This LavaLink node no longer exists" }]))
 					const stat = node.stats
