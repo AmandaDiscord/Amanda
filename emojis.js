0b5ddb3de02cf9247c10807ce91f24ce25791bbb
Papi <bradleyjplatt@yahoo.com>
2021-11-02 13:40:51 -0700

    snap

--- emojis.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	"discoin": "<a:Discoin:422523472128901140>",
-	"discoinurl": "https://cdn.discordapp.com/emojis/422523472128901140.gif?v=1",
-	"bot": "<:bot:412413027565174787>",
-	"boturl": "https://cdn.discordapp.com/emojis/412413027565174787.png?v=1",
-	"bl": "<:bl:616834265732349953>"
-}
-
