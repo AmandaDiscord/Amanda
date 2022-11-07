8856973c2205bf7a309fa3d58339751d718a4eec
Papa <bradleyjplatt@yahoo.com>
2022-11-06 21:06:37 -0700

    I am very bad at coding

--- source/constants.ts
+++ source/constants.ts
@@ -48,7 +48,7 @@ export const standard_embed_color = 0x2f3136
 export const discord_background_color = 0x36393f
 export const chewey_api = "https://api.chewey-bot.top"
 export const lavalinkNodes = [] as Array<import("./types").InferModelDef<typeof import("./utils/orm")["db"]["tables"]["lavalink_nodes"]> & { regions: Array<string>; id: string; resumeKey?: string; resumeTimeout?: number; password: string }>
-const devNode = { host: "localhost", port: 10402, invidious_origin: "http://amanda.moe:3000", enabled: 1, search_with_invidious: 0, name: "DEV", regions: ["us-west"], password: config.lavalink_password, id: "dev", resumeKey: `${Buffer.from(config.bot_token?.split(".")[0], "base64").toString("utf8")}/dev`, resumeTimeout: 75 }
+const devNode = { host: "localhost", port: 10402, invidious_origin: "http://amanda.moe:3000", enabled: 1, search_with_invidious: 0, name: "DEV", regions: ["us-west"], password: config.lavalink_password, id: "dev", resumeKey: `${Buffer.from(config.bot_token?.split(".")[0] || "", "base64").toString("utf8")}/dev`, resumeTimeout: 75 }
 if (config.is_dev_env) lavalinkNodes.push(devNode)
 
 export const frisky_placeholder = `${baseURL}/images/frisky-new.webp`
