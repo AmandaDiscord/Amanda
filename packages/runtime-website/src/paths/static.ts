653a5fa49d0e4655b5b4bbc0ffe1300d4455a8f7
Papa <bradleyjplatt@yahoo.com>
2023-10-18 11:24:50 -0700

    traffic advice

--- packages/runtime-website/src/paths/static.ts
+++ packages/runtime-website/src/paths/static.ts
@@ -7,3 +7,21 @@ const utils: typeof import("../utils") = sync.require("../utils")
 server.get("/*", (res, req) => utils.streamFile(req.getUrl(), res, req.getHeader("accept"), req.getHeader("if-modified-since")))
 
 server.get("/", (res, req) => utils.streamFile("index.html", res, req.getHeader("accept"), req.getHeader("if-modified-since")))
+
+
+// fake fs paths
+server.get("/.well-known/traffic-advice", res => {
+	const data = [{
+		user_agent: "prefetch-proxy",
+		google_prefetch_proxy_eap: {
+			fraction: 1.0
+		}
+	}]
+	const payload = JSON.stringify(data)
+
+	res
+		.writeStatus("200")
+		.writeHeader("Content-Type", "application/trafficadvice+json") // done because of fancy Content-Type
+		.writeHeader("Content-Length", String(Buffer.byteLength(payload)))
+		.end(payload)
+})
