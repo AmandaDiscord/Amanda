7a9cc322e8de8230cd527e8325ec7d519650234a
Papi <bradleyjplatt@yahoo.com>
2025-01-02 12:24:02 -0700

    Stringify Date.now() in upsert

--- packages/runtime-worker/src/commands/money.ts
+++ packages/runtime-worker/src/commands/money.ts
@@ -545,7 +545,7 @@ commands.assign([
 			canvas.drawImage(hands, 0, 0)
 
 			await moneyManager.awardAmount(cmd.author.id, winnings, "Amanda fortune")
-			await sql.orm.upsert("daily_cooldown", { user_id: cmd.author.id, last_claim: Date.now() })
+			await sql.orm.upsert("daily_cooldown", { user_id: cmd.author.id, last_claim: String(Date.now()) })
 
 			return client.snow.interaction.editOriginalInteractionResponse(cmd.application_id, cmd.token, { files: [{ name: "fortune.png", file: canvas.canvas.toBuffer("image/png") }] })
 		}
